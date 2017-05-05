86b4d3aab5900afce0cf2a4ffe6d15bbf7ea8344
lkotlewski <luckyyplayer@gmail.com>
2017-05-05 15:15:07 +0200

    Pierwsza wersja sieci neuronowej, dane X oraz y przechowywane sa w numpy.array, dodano standaryzacje y

--- NuclearWeb/DataCollector.py
+++ NuclearWeb/DataCollector.py
@@ -1,12 +1,12 @@
 import csv as csv
 import numpy as np
+from numpy import double
 '''
 Created on 20.04.2017
 
 @author: Konrad
+from test.test_zipfile import DATAFILES_DIR
 '''
-from numpy import double
-'''from test.test_zipfile import DATAFILES_DIR'''
 
 class DataCollector(object):
     '''
@@ -14,54 +14,49 @@ class DataCollector(object):
     '''
 
     def __init__(self):
-        self.x=[]
-        self.d=[]
+        self.X=[]
+        self.y=[]
         self.dataFile=open('snbDANE.csv','r')
         dataReader = csv.reader(self.dataFile)
         self.titles = next(dataReader)
         for row in dataReader:
-            self.x.append(list(map(double,row[0:len(row)-1])))
-            self.d.append(double(row[len(row)-1]))
+            self.X.append(list(map(double, row[0:len(row) - 1])))
+            self.y.append(double(row[len(row) - 1]))
         '''self.printData(self.x)'''
         '''self.findMax()'''
+        self.X = np.array(self.X)
+        self.y = np.array(self.y)
         self.standarization()
         '''print(self.d)
         self.printData(self.x)'''
-    pass
 
     def findMax(self):
         max=[]
-        for i in range(len(self.x[0])):
-            a = np.array(self.x,dtype=double)
+        for i in range(len(self.X[0])):
+            a = np.array(self.X, dtype=double)
             max.append(a[:,i].max())
         '''print(max)'''
-    pass
 
     def printData(self,data):
         for i in range(len(data)):
             print(data[i][:])
-    pass
 
     def standarization(self):
-        max=[]
-        '''print("Dlugosc: " + str(len(self.x[0])))'''
-        for i in range(len(self.x[0])):
-            '''print("Robie kolumne: " + str(i)) '''
-            a = np.array(self.x,dtype=double)
-            max = a[:,i].max()
-            for j in range(len(self.x)):
-                self.x[j][i] = self.x[j][i]/max
-        '''print(self.x)'''
-    pass
+        for i in range(len(self.X[0])):
+            #X_array = np.array(self.X, dtype=double)
+            #y_array = np.array(self.y, dtype=double)
+            max_value_X = self.X[:, i].max()
+            max_value_y = self.y.max()
+            for j in range(len(self.X)):
+                self.X[j][i] = self.X[j][i] / max_value_X
+        self.y = self.y / max_value_y
 
     def getX(self):
-        return self.x
-    pass
+        return self.X
 
-    def getD(self):
-        return self.d
-    pass
+
+    def getY(self):
+        return self.y
 
     def getHeads(self):
         return self.titles
-    pass
--- NuclearWeb/Main.py
+++ NuclearWeb/Main.py
@@ -1,9 +1,14 @@
 from DataCollector import DataCollector
+from NeuralWeb import NeuralNetMLP
 '''
 Created on 20.04.2017
 
 @author: Konrad
 '''
 def main():
-        new = DataCollector()
-if  __name__ =='__main__':main()
+        data_collector = DataCollector()
+        net = NeuralNetMLP()
+        net.fit(data_collector.X, data_collector.y)
+
+if  __name__ =='__main__':
+    main()
