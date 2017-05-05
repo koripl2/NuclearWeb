9a69553659b2c43ff40712273087deefb820d6b1
lkotlewski <luckyyplayer@gmail.com>
2017-05-05 17:21:36 +0200

    Porownanie wyjsc sieci po poszczegolnych pobudzeniach i oczekiwanych odpowiedzi na te pobudzenia

--- NuclearWeb/Main.py
+++ NuclearWeb/Main.py
@@ -1,5 +1,7 @@
+import numpy as np
 from DataCollector import DataCollector
 from NeuralWeb import NeuralNetMLP
+import matplotlib.pyplot as plt
 '''
 Created on 20.04.2017
 
@@ -7,8 +9,12 @@ Created on 20.04.2017
 '''
 def main():
         data_collector = DataCollector()
-        net = NeuralNetMLP()
+        net = NeuralNetMLP(n_hidden=8)
         net.fit(data_collector.X, data_collector.y)
-
+        y_net = np.array(0)
+        _, _, _, _, y_net = net._feedforward(data_collector.X, net.w1, net.w2)
+        plt.plot(data_collector.y, color='green')
+        plt.plot(np.transpose(y_net), color='blue')
+        plt.show()
 if  __name__ =='__main__':
     main()
